7e961d649479c2de583f8a37b9595bf29b20bdd7
Karl Molina <karlgmolina@gmail.com>
2018-11-25 22:52:20 -0700

    Add delay

--- swamp3/swamp3.ino
+++ swamp3/swamp3.ino
@@ -5,15 +5,19 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(WATER_SENSOR, INPUT);
   pinMode(RELAY, OUTPUT);
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  //Serial.begin(250000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(digitalRead(WATER_SENSOR));
+  //Serial.println(digitalRead(WATER_SENSOR));
   if (digitalRead(WATER_SENSOR) == HIGH) {
     digitalWrite(RELAY, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(10000);
   } else {
     digitalWrite(RELAY, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
   }
 }
